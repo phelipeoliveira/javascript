bf4852cf2a55a07dcf39026933fd88839dc464fa
Phelipe Oliveira <105466788+phelipeoliveira@users.noreply.github.com>
2022-05-28 20:58:54 -0300

    atualização vs1.0.0

---
+++ animacoes/dna/script.js
@@ -0,0 +1,13 @@
+// mouseX and mouseY
+
+strokeWeight(-4);
+stroke(31, 31, 31);
+fill(255, 255, 255);
+
+draw = function() {
+    
+    //background(255, 255, 255);
+    
+    ellipse(mouseX, mouseY, 14, 14);
+    
+};
---
+++ animacoes/mouse-maluco/script.js
@@ -0,0 +1,7 @@
+
+
+draw = function() {
+    fill(9, 0, 255);
+    stroke(255, 0, 0);
+    ellipse(mouseX, mouseY, 30, 30);
+};
