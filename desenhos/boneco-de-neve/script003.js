e03105631b33c2ce6c67f7c2ec938cd804cd7ab5
Phelipe Oliveira <105466788+phelipeoliveira@users.noreply.github.com>
2022-06-07 19:55:52 -0300

    atualização vs1.0.2

---
+++ desenhos/boneco-de-neve/script003.js
@@ -0,0 +1,15 @@
+background(35, 207, 190);
+// The ground
+fill(34, 255, 0);
+rect(0, 300, 400, 100);  
+
+// The sun
+fill(251, 255, 0);
+ellipse(80, 64, 100, 100);  
+
+// The snowman
+fill(255, 255, 255);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
+
